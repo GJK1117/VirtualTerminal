c79f5d402db4093fdb3b7c0e825bcc8b89664c59
juun0-h <juun0.han@gmail.com>
2024-07-09 16:09:39 +0900

    Revert "장고 프로젝트 생성 및 구조화(++ 'cd', 'pwd', 'mkdir' 로직 구현)"

--- backend/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from api.models import Item
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Item
-        fields=['id', 'title', 'content', 'created_at', 'updated_at']
